a92a045c9c486c96bf3701f978bb456adf64fc84
Carey Jo <wansoo.carey.jo@gmail.com>
2025-10-30 07:12:59 +0900

    chore: bump app version for store build

--- constants/version.ts
+++ constants/version.ts
@@ -3,4 +3,4 @@
  * Increment this by 1 for each build when app version changes
  * Combined with app.json version (e.g., 1.0.3.0)
  */
-export const BUILD_NUMBER = 2;
+export const BUILD_NUMBER = 0;
